71fbf72e905f424515693b02182bc6ec11c2f28b
karina <kcanelacobos@gmail.com>
2023-03-15 18:14:02 -0600

    Se modifico la seccion Experiencia Profesional

--- validacion.js
+++ validacion.js
@@ -1,1 +1,23 @@
 //Haz tú validación en javascript acá
+var boxes = document.querySelectorAll(".experiencie__box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    // console.log(window.innerHeight);
+    // console.log(boxes[7].getBoundingClientRect().top);
+
+    const triggerBottom = (window.innerHeight / 5) * 4;
+
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            box.classList.add("show");
+        } else {
+            box.classList.remove("show");
+        }
+    });
+}
