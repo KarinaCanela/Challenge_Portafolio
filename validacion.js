4fc75dabc8f38aa06e14a3bdf684c1d99ba50079
karina <kcanelacobos@gmail.com>
2023-03-15 23:54:52 -0600

    Se modifico el estilo del proyecto

--- validacion.js
+++ validacion.js
@@ -6,9 +6,6 @@ window.addEventListener("scroll", checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
-    // console.log(window.innerHeight);
-    // console.log(boxes[7].getBoundingClientRect().top);
-
     const triggerBottom = (window.innerHeight / 5) * 4;
 
     boxes.forEach((box) => {
